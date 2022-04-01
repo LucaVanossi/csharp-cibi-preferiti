1678e7ad6d643479bf0b5d6f66d3ede5a8de538a
LucaVanossi <vanossi.luca13@gmail.com>
2022-04-01 16:55:48 +0200

    Esercizio con bonus ok

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] CibiPreferiti = { "Pizza", "Pasta", "Riso", "Salumi", "Torta" };
+
+Console.WriteLine("Il numero dei cibi inseriti è :" + CibiPreferiti.Length);
+
+for (int i = 0; i < CibiPreferiti.Length; i++)
+{
+    Console.WriteLine(i +1 + ". " + CibiPreferiti[i]);
+    
+}
+
+Console.WriteLine("Il mio cibo preferito è : " + CibiPreferiti[0]);
+Console.WriteLine("Tra i miei preferiti l'ultimo è : " + CibiPreferiti[4]);
+
+int calcolo = CibiPreferiti.Length / 2;
+
+if (CibiPreferiti.Length %2 ==0)
+{
+    Console.WriteLine("Il cibo a metà classifica è: " + CibiPreferiti[calcolo]);
+} 
+else
+{
+    Console.WriteLine("I cibi a metà classifica sono: " + CibiPreferiti[calcolo -1] + " e " +
+        CibiPreferiti[calcolo]);
+}
